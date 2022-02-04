47e4098869d21509cb77fe5be09f80af89c585a9
lino31 <35493399+lino31@users.noreply.github.com>
2022-02-04 01:16:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import altair as alt
-
+import matplotlib.pyplot as plt
 
 
 st.title("Smart Bridge Sustainment in the Circular Economy")
